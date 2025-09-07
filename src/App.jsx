442623f9e8a52831eed1c88434980f515175ff8d
Anjali-Chouhan07 <anjalichouhan2277@gmail.com>
2025-09-07 12:32:06 +0530

    Remove lanspo.svg favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Investors from "./components/Investors";
 import Services from "./components/Services";
 import ContactUs from "./components/ContactUs";
@@ -9,7 +9,7 @@ import styles from "./style";
 import PrivacyPolicy from "./components/PrivacyPolicy";
 import Terms from "./components/TermsAndCondition";
 import Disclaimer from "./components/Disclaimer";
-import ScrollToTop from "./ScrollToTop";   
+import ScrollToTop from "./ScrollToTop";
 
 import {
   Billing,
@@ -24,57 +24,56 @@ import {
   Hero,
 } from "./components";
 
-import Layout from "./Layout"; 
+import Layout from "./Layout";
 
 const App = () => {
   return (
-    <Router>
-      <ScrollToTop />   
-      <div className="bg-primary w-full overflow-hidden">
-        {/* Navbar */}
-        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-          <div className={`${styles.boxWidth}`}>
-            <Navbar />
-          </div>
+    <div className="bg-primary w-full overflow-hidden">
+      <ScrollToTop />
+      
+      {/* Navbar */}
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
         </div>
-
-        <Routes>
-          {/* Landing Page */}
-          <Route
-            path="/"
-            element={
-              <Layout>
-                <div className={`bg-primary ${styles.flexStart}`}>
-                  <div className={`${styles.boxWidth}`}>
-                    <Hero />
-                  </div>
-                </div>
-
-                <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-                  <div className={`${styles.boxWidth}`}>
-                    <Business />
-                    <Billing />
-                    <CardDeal />
-                  </div>
-                </div>
-              </Layout>
-            }
-          />
-
-          {/* Other Pages */}
-          <Route path="/About" element={<Layout><About /></Layout>} />
-          <Route path="/contact-us" element={<Layout><ContactUs /></Layout>} />
-          <Route path="/blog" element={<Layout><Blog /></Layout>} />
-          <Route path="/careers" element={<Layout><Careers /></Layout>} />
-          <Route path="/privacy-policy" element={<Layout><PrivacyPolicy /></Layout>} />
-          <Route path="/terms" element={<Layout><Terms /></Layout>} />
-          <Route path="/disclaimer" element={<Layout><Disclaimer /></Layout>} />
-          <Route path="/services" element={<Layout><Services /></Layout>} />
-          <Route path="/investors" element={<Layout><Investors /></Layout>} />
-          <Route path="/group" element={<Layout><Group /></Layout>} />
-        </Routes>
       </div>
-    </Router>
+
+      <Routes>
+        {/* Landing Page */}
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <div className={`bg-primary ${styles.flexStart}`}>
+                <div className={`${styles.boxWidth}`}>
+                  <Hero />
+                </div>
+              </div>
+
+              <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+                <div className={`${styles.boxWidth}`}>
+                  <Business />
+                  <Billing />
+                  <CardDeal />
+                </div>
+              </div>
+            </Layout>
+          }
+        />
+
+        {/* Other Pages */}
+        <Route path="/About" element={<Layout><About /></Layout>} />
+        <Route path="/contact-us" element={<Layout><ContactUs /></Layout>} />
+        <Route path="/blog" element={<Layout><Blog /></Layout>} />
+        <Route path="/careers" element={<Layout><Careers /></Layout>} />
+        <Route path="/privacy-policy" element={<Layout><PrivacyPolicy /></Layout>} />
+        <Route path="/terms" element={<Layout><Terms /></Layout>} />
+        <Route path="/disclaimer" element={<Layout><Disclaimer /></Layout>} />
+        <Route path="/services" element={<Layout><Services /></Layout>} />
+        <Route path="/investors" element={<Layout><Investors /></Layout>} />
+        <Route path="/group" element={<Layout><Group /></Layout>} />
+      </Routes>
+    </div>
   );
 };
 
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Lanspo-Private-Limited-Web-page/"    
+  base: "/Lanspo-Private-Limited-Web-page/", 
 })
